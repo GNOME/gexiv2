89d5cf9a6678ab36c3ccecd31f0329dbb1364261
Jens Georg <mail@jensge.org>
2016-04-23 10:49:06 +0200

    initial gtk-doc support
    Signed-off-by: Jens Georg <mail@jensge.org>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-mkdir -p m4
-autoreconf --force --install --verbose
+olddir=$(pwd)
+
+cd $srcdir
+
+(test -f configure.ac) || {
+        echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
+        exit 1
+}
+
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+
+aclocal --install || exit 1
+gtkdocize || exit 1
+autoreconf --verbose --force --install || exit 1
+
+cd "$olddir"
