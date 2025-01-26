e5b1c84363ece90317b9459758d6320696b1c927
Jens Georg <mail@jensge.org>
2025-01-26 20:12:03 +0100

    wip

--- .gitlab-ci/test-ucrt.sh
+++ .gitlab-ci/test-ucrt.sh
@@ -5,17 +5,20 @@ MW=mingw-w64-ucrt-x86_64-
 pacman -S --needed base-devel \
     ${MW}toolchain \
     ${MW}gcc \
+    ${MW}g++ \
     ${MW}gobject-introspection \
     ${MW}gtk-doc \
     ${MW}meson \
     ${MW}ninja \
     ${MW}python-gobject \
-    ${MW}vala
+    ${MW}vala \
+    ${MW}cmake \
+    ${MW}pkg-config \
 
 git clone https://github.com/exiv2/exiv2
 mkdir exiv2-build
-meson setup exiv2-build exiv2 --prefix "${PWD}"/exiv2-install
-ninja -C exiv2-build install
-PKG_CONFIG_PATH=${PWD}/exiv2-install/share/pkgconfig:${PKG_CONFIG_PATH} meson setup build --buildtype debug
-ninja -C build
-meson test -C build
+/usr/bin/meson setup exiv2-build exiv2 --prefix "${PWD}"/exiv2-install
+/usr/bin/ninja -C exiv2-build install
+PKG_CONFIG_PATH=${PWD}/exiv2-install/share/pkgconfig:${PKG_CONFIG_PATH} /usr/bin/meson setup build --buildtype debug
+/usr/bin/ninja -C build
+/usr/bin/meson test -C build
