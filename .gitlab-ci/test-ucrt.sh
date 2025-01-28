e0c3f3baf69735b90e27e95036663b4eb10163e9
Jens Georg <mail@jensge.org>
2025-01-28 08:18:18 +0000

    wip: ucrt64 build

--- .gitlab-ci/test-ucrt.sh
+++ .gitlab-ci/test-ucrt.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/bash -e
 
-MW=mingw-w64-ucrt-x86_64-
+pacman --noconfirm -Suy
 
 pacman -S --needed base-devel \
-    ${MW}toolchain \
-    ${MW}gcc \
-    ${MW}g++ \
-    ${MW}gobject-introspection \
-    ${MW}gtk-doc \
-    ${MW}meson \
-    ${MW}ninja \
-    ${MW}python-gobject \
-    ${MW}vala \
-    ${MW}cmake \
-    ${MW}pkg-config \
+    ${MINGW_PACKAGE_PREFIX}-toolchain \
+    ${MINGW_PACKAGE_PREFIX}-gcc \
+    ${MINGW_PACKAGE_PREFIX}-g++ \
+    ${MINGW_PACKAGE_PREFIX}-gobject-introspection \
+    ${MINGW_PACKAGE_PREFIX}-gtk-doc \
+    ${MINGW_PACKAGE_PREFIX}-meson \
+    ${MINGW_PACKAGE_PREFIX}-ninja \
+    ${MINGW_PACKAGE_PREFIX}-python-gobject \
+    ${MINGW_PACKAGE_PREFIX}-vala \
+    ${MINGW_PACKAGE_PREFIX}-cmake \
+    ${MINGW_PACKAGE_PREFIX}-pkg-config \
 
 git clone https://github.com/exiv2/exiv2
 mkdir exiv2-build
-/usr/bin/meson setup exiv2-build exiv2 --prefix "${PWD}"/exiv2-install
-/usr/bin/ninja -C exiv2-build install
-PKG_CONFIG_PATH=${PWD}/exiv2-install/share/pkgconfig:${PKG_CONFIG_PATH} /usr/bin/meson setup build --buildtype debug
-/usr/bin/ninja -C build
-/usr/bin/meson test -C build
+meson setup exiv2-build exiv2 --prefix "${PWD}"/exiv2-install
+ninja -C exiv2-build install
+PKG_CONFIG_PATH=${PWD}/exiv2-install/share/pkgconfig:${PKG_CONFIG_PATH} meson setup build --buildtype debug
+ninja -C build
+meson test -C build
