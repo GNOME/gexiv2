0d5626e323747cd6dd6ae84c1b07d084a6b8d257
Jens Georg <mail@jensge.org>
2025-01-28 19:14:46 +0000

    Update test-ucrt.sh

--- .gitlab-ci/test-ucrt.sh
+++ .gitlab-ci/test-ucrt.sh
@@ -12,10 +12,15 @@ pacman --noconfirm -S --needed base-devel \
     ${MINGW_PACKAGE_PREFIX}-python-gobject \
     ${MINGW_PACKAGE_PREFIX}-vala \
     ${MINGW_PACKAGE_PREFIX}-cmake \
-    ${MINGW_PACKAGE_PREFIX}-pkg-config \
+    ${MINGW_PACKAGE_PREFIX}-pkg-config
 
 git clone https://github.com/exiv2/exiv2
 mkdir exiv2-build
+
+ls -l /ucrt64/bin/cmake 
+
+/ucrt64/bin/cmake --help
+
 /ucrt64/bin/cmake -B exiv2-build -S exiv2 -GNinja \
     -DCMAKE_INSTALL_PREFIX="${PWD}"/exiv2-install \
     -DCMAKE_BUILD_TYPE=Debug \
