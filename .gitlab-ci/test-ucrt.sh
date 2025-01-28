12312e9178a89401c90817ec1f3e0d8c47a26d2f
Jens Georg <mail@jensge.org>
2025-01-28 09:58:17 +0000

    switch to cmake for exiv2

--- .gitlab-ci/test-ucrt.sh
+++ .gitlab-ci/test-ucrt.sh
@@ -16,7 +16,15 @@ pacman --noconfirm -S --needed base-devel \
 
 git clone https://github.com/exiv2/exiv2
 mkdir exiv2-build
-meson setup exiv2-build exiv2 --prefix "${PWD}"/exiv2-install
+cmake -B exiv2-build -S exiv2 -GNinja \
+    -DCMAKE_INSTALL_PREFIX="${PWD}"/exiv2-install \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DEXIV2_BUILD_EXIV2_COMMAND=OFF \
+    -DEXIV2_BUILD_SAMPLES=OFF \
+    -DEXIV2_BUILD_UNIT_TESTS=OFF \
+    -DEXIV2_ENABLE_BMFF=ON \
+    -DEXIV2_ENABLE_INIH=OFF
+
 ninja -C exiv2-build install
 PKG_CONFIG_PATH=${PWD}/exiv2-install/share/pkgconfig:${PKG_CONFIG_PATH} meson setup build --buildtype debug
 ninja -C build
