ee532d6c1a00b223b86d1bdb0f0fa10bccd1c3ee
Jens Georg <mail@jensge.org>
2025-01-29 10:42:02 +0100

    Use exiv2 pacakge from msys

--- .gitlab-ci/test-ucrt.sh
+++ .gitlab-ci/test-ucrt.sh
@@ -11,26 +11,9 @@ pacman --noconfirm -S --needed base-devel \
     ${MINGW_PACKAGE_PREFIX}-ninja \
     ${MINGW_PACKAGE_PREFIX}-python-gobject \
     ${MINGW_PACKAGE_PREFIX}-vala \
-    ${MINGW_PACKAGE_PREFIX}-cmake \
+    ${MINGW_PACKAGE_PREFIX}-exiv2 \
     ${MINGW_PACKAGE_PREFIX}-pkg-config
 
-git clone https://github.com/exiv2/exiv2
-mkdir exiv2-build
-
-ls -l /ucrt64/bin/cmake 
-
-/ucrt64/bin/cmake --help
-
-/ucrt64/bin/cmake -B exiv2-build -S exiv2 -GNinja \
-    -DCMAKE_INSTALL_PREFIX="${PWD}"/exiv2-install \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DEXIV2_BUILD_EXIV2_COMMAND=OFF \
-    -DEXIV2_BUILD_SAMPLES=OFF \
-    -DEXIV2_BUILD_UNIT_TESTS=OFF \
-    -DEXIV2_ENABLE_BMFF=ON \
-    -DEXIV2_ENABLE_INIH=OFF
-
-ninja -C exiv2-build install
-PKG_CONFIG_PATH=${PWD}/exiv2-install/share/pkgconfig:${PKG_CONFIG_PATH} meson setup build --buildtype debug
+meson setup build --buildtype debug -Dtests=true
 ninja -C build
 meson test -C build
