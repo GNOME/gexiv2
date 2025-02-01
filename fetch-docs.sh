ad1b5cb402600acb739b86fc2db571f15850c334
Jens Georg <mail@jensge.org>
2025-02-01 16:12:30 +0100

    Add pages CI job

---
+++ fetch-docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash -e
+
+DOC_DIR=public/docs
+REFS="
+master
+gexiv2-0.14
+"
+
+mkdir -p $DOC_DIR
+
+IFS='
+'
+
+
+curl -L --output README.md "https://gitlab.gnome.org/GNOME/gexiv2/-/raw/master/README.md?ref_type=heads"
+pandoc -s README.md -o public/index.html --metadata=pagetitle:"GExiv2" --css=style.css
+pandoc -s versions.md -o $DOC_DIR/index.html --metadata=pagetitle:"GEXiv2 Versions" --css=style.css
+
+for REF in $REFS; do 
+    API_VERSION=${REF//gexiv2-/}
+    curl -L --output "$REF.zip" "https://gitlab.gnome.org/GNOME/gexiv2/-/jobs/artifacts/$REF/download?job=docs"
+    unzip -d "$REF" "$REF.zip"
+    mv "$REF/docs" "$DOC_DIR/$API_VERSION"
+    rm "$REF.zip"
+    rm -rf "$REF"
+done
---
+++ versions.md
@@ -0,0 +1,6 @@
+# Available documentation
+
+* [Latest stable version](https://gnome.pages.gitlab.gnome.org/gexiv2/doc/latest)
+* [Documentation for GExiv2 0.14](https://gnome.pages.gitlab.gnome.org/gexiv2/docs/gexiv2-0.14)
+* [Documentation for GExiv2 0.12](https://gnome.pages.gitlab.gnome.org/gexiv2/docs/gexiv2-0.12)
+* [Upcoming development version](https://gnome.pages.gitlab.gnome.org/gexiv2/doc/master)
