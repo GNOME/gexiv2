94695c796b4651eb6c5eb7da017e5813ce9908d6
Jens Georg <mail@jensge.org>
2025-05-25 10:49:29 +0200

    fix: Build complete .def file in tool

--- build-aux/update-def-file.sh
+++ build-aux/update-def-file.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-nm -gD "$1" | grep " T " | cut -f3 -d " " | sort | uniq > "$2"
+set -e
+
+echo $1 $2
+
+echo "EXPORTS" > "$2"
+nm -gD "$1" | grep " T " | cut -f3 -d " " | sort | uniq >> "$2"
